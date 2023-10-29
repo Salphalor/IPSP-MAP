7c1d07b70a13a580ffd4930fb0b136a0e64d7e33
Salphalor <42503231+Salphalor@users.noreply.github.com>
2023-10-29 13:21:49 +0100

    Add files via upload

---
+++ IPSP_Info.js
@@ -0,0 +1,14 @@
+var IPSP_Info = {
+  "Egypt": {
+    "No_Students": 1,
+    "contact": "Max Al Muster"
+  },
+  "Canada": {
+    "No_Students": 1,
+    "contact": "Robin Scherbatsky"
+  },
+  "Ukraine": {
+  	"No_Students": "?",
+    "contact": "Volodymyr Agafonkin"
+  }
+}
